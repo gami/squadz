96f99a88518848d2f171fb29e1dc8d97479038c7
Masaki Takegami <takegamm@gmail.com>
2021-06-18 22:17:20 +0900

    move counter to ui & add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:squadz/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('Boot main widget', (WidgetTester tester) async {
+    const testKey = Key('test');
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(const Squadz(
+      key: testKey,
+    ));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byKey(testKey), findsOneWidget);
   });
 }
